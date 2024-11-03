d8c62a532bce4b98dbb0d51219dcbb668b091a76
Nanqipro <2280285631@qq.com>
2024-11-03 23:46:36 +0800

    20241103

---
+++ build01/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Bisson/tc.cu" "CMakeFiles/main.dir/src/approach/Bisson/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Bisson/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Fox/tc.cu" "CMakeFiles/main.dir/src/approach/Fox/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Fox/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Green/clusteringCount.cu" "CMakeFiles/main.dir/src/approach/Green/clusteringCount.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Green/clusteringCount.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Green/gpurun.cu" "CMakeFiles/main.dir/src/approach/Green/gpurun.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Green/gpurun.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Green/main.cu" "CMakeFiles/main.dir/src/approach/Green/main.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Green/main.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Green/param.cu" "CMakeFiles/main.dir/src/approach/Green/param.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Green/param.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/GroupTC-HASH/tc.cu" "CMakeFiles/main.dir/src/approach/GroupTC-HASH/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/GroupTC-HASH/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/GroupTC-OPT/tc.cu" "CMakeFiles/main.dir/src/approach/GroupTC-OPT/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/GroupTC-OPT/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/GroupTC/tc.cu" "CMakeFiles/main.dir/src/approach/GroupTC/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/GroupTC/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/H-INDEX/tc.cu" "CMakeFiles/main.dir/src/approach/H-INDEX/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/H-INDEX/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Hu/tc.cu" "CMakeFiles/main.dir/src/approach/Hu/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Hu/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Polak/tc.cu" "CMakeFiles/main.dir/src/approach/Polak/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/Polak/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/TC-Check/tc.cu" "CMakeFiles/main.dir/src/approach/TC-Check/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/TC-Check/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/TRUST/tc.cu" "CMakeFiles/main.dir/src/approach/TRUST/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/TRUST/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/TriCore/tc.cu" "CMakeFiles/main.dir/src/approach/TriCore/tc.cu.o" "gcc" "CMakeFiles/main.dir/src/approach/TriCore/tc.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/datatransfer/csr2dcsr_data_transfer.cu" "CMakeFiles/main.dir/src/datatransfer/csr2dcsr_data_transfer.cu.o" "gcc" "CMakeFiles/main.dir/src/datatransfer/csr2dcsr_data_transfer.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/datatransfer/csr2rid_dcsr_data_transfer.cu" "CMakeFiles/main.dir/src/datatransfer/csr2rid_dcsr_data_transfer.cu.o" "gcc" "CMakeFiles/main.dir/src/datatransfer/csr2rid_dcsr_data_transfer.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/datatransfer/csr2trust_dcsr_data_transfer.cu" "CMakeFiles/main.dir/src/datatransfer/csr2trust_dcsr_data_transfer.cu.o" "gcc" "CMakeFiles/main.dir/src/datatransfer/csr2trust_dcsr_data_transfer.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/datatransfer/data_transfer.cu" "CMakeFiles/main.dir/src/datatransfer/data_transfer.cu.o" "gcc" "CMakeFiles/main.dir/src/datatransfer/data_transfer.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/graph/cuda_graph_comm.cu" "CMakeFiles/main.dir/src/graph/cuda_graph_comm.cu.o" "gcc" "CMakeFiles/main.dir/src/graph/cuda_graph_comm.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/graph/gpu_graph.cu" "CMakeFiles/main.dir/src/graph/gpu_graph.cu.o" "gcc" "CMakeFiles/main.dir/src/graph/gpu_graph.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/main.cu" "CMakeFiles/main.dir/src/main.cu.o" "gcc" "CMakeFiles/main.dir/src/main.cu.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Green/clusteringCount.cpp" "CMakeFiles/main.dir/src/approach/Green/clusteringCount.cpp.o" "gcc" "CMakeFiles/main.dir/src/approach/Green/clusteringCount.cpp.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Green/csrdatacreate.cpp" "CMakeFiles/main.dir/src/approach/Green/csrdatacreate.cpp.o" "gcc" "CMakeFiles/main.dir/src/approach/Green/csrdatacreate.cpp.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/approach/Green/graphRead.cpp" "CMakeFiles/main.dir/src/approach/Green/graphRead.cpp.o" "gcc" "CMakeFiles/main.dir/src/approach/Green/graphRead.cpp.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/comm/config_comm.cpp" "CMakeFiles/main.dir/src/comm/config_comm.cpp.o" "gcc" "CMakeFiles/main.dir/src/comm/config_comm.cpp.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/comm/constant_comm.cpp" "CMakeFiles/main.dir/src/comm/constant_comm.cpp.o" "gcc" "CMakeFiles/main.dir/src/comm/constant_comm.cpp.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/graph/cpu_graph.cpp" "CMakeFiles/main.dir/src/graph/cpu_graph.cpp.o" "gcc" "CMakeFiles/main.dir/src/graph/cpu_graph.cpp.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/ini/INIReader.cpp" "CMakeFiles/main.dir/src/ini/INIReader.cpp.o" "gcc" "CMakeFiles/main.dir/src/ini/INIReader.cpp.o.d"
+  "/app/ZZQ/zj/gitlocal/github_TC/src/ini/ini.cpp" "CMakeFiles/main.dir/src/ini/ini.cpp.o" "gcc" "CMakeFiles/main.dir/src/ini/ini.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
