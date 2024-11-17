76a566fc44a7bf9f9c197a6aa44210dabd7df66b
Nanqipro <2280285631@qq.com>
2024-11-17 11:32:16 +0800

    20241117

--- data_getter/g500/run.sh
+++ data_getter/g500/run.sh
@@ -119,3 +119,22 @@ chmod +x seq-csr
 ./seq-csr  /app/ZZQ/zj/gitlocal/github_TC/data/g500_dataset/cluster3-s17-e64/edges.bin  -R  -s 17  -e 64   &  
 ./seq-csr  /app/ZZQ/zj/gitlocal/github_TC/data/g500_dataset/cluster3-s17-e256/edges.bin  -R  -s 17  -e 256   &  
 ./seq-csr  /app/ZZQ/zj/gitlocal/github_TC/data/g500_dataset/cluster3-s17-e1024/edges.bin  -R  -s 17  -e 1024   &  
+
+# zj_new
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e2/edges.bin  -R  -s 20  -e 2   &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e4/edges.bin  -R  -s 20 -e 4 &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e8/edges.bin  -R  -s 20 -e 8  &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e16/edges.bin  -R  -s 20  -e 16  &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e32/edges.bin  -R  -s 20  -e 32  &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e64/edges.bin  -R  -s 20  -e 64 &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e128/edges.bin  -R  -s 20 -e 128 &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster2-s20-e256/edges.bin  -R  -s 20 -e 256   &  
+
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s17-e32/edges.bin  -R  -s 17  -e 32 &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s18-e32/edges.bin  -R  -s 18  -e 32 &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s19-e32/edges.bin  -R  -s 19  -e 32  &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s20-e32/edges.bin  -R  -s 20  -e 32  &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s21-e32/edges.bin  -R  -s 21  -e 32 &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s22-e32/edges.bin  -R  -s 22  -e 32   &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s23-e32/edges.bin  -R  -s 23  -e 32 &  
+nohup ./seq-csr  /app/ZJ/gitlocal/github_TC/data/g500_dataset/cluster4-s24-e32/edges.bin  -R  -s 24  -e 32  &  
